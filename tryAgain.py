fd5aae21b0f79fe10fd808cc3b59bf9d7bee0c65
Karl Lu <taishan02@yahoo.com>
2026-04-28 16:23:13 -0700

    more message.

--- tryAgain.py
+++ tryAgain.py
@@ -9,9 +9,9 @@
 def main():
     print("Hello World")
     print("Welcome to Python!")
+    print("Second time is a charm!")
 
 # This line ensures the main function runs when the script is executed
 if __name__ == "__main__":
     main()
 
-    
