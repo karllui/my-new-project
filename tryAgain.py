12572dae32e0261508a469493fc83ea2d12483f6
Karl Lu <taishan02@yahoo.com>
2026-04-28 14:53:10 -0700

    Initial commit

---
+++ tryAgain.py
@@ -0,0 +1,17 @@
+#
+# Hello World
+# 
+# Prompt
+# write a python program that prints "Hello World"
+#
+#####################################################
+
+def main():
+    print("Hello World")
+    print("Welcome to Python!")
+
+# This line ensures the main function runs when the script is executed
+if __name__ == "__main__":
+    main()
+
+    
